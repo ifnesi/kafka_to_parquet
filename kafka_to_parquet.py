7a0f894f52607b55fb58484735136efa7fdd04bf
ifnesi <inesi@confluent.io>
2024-01-20 15:48:15 +0000

    Minor

--- kafka_to_parquet.py
+++ kafka_to_parquet.py
@@ -32,7 +32,8 @@ def main(args):
     if args.delete:
         for root, dirs, files in os.walk(utils.DUCKDB_DATA_FOLDER, topdown=False):
             for name in files:
-                os.remove(os.path.join(root, name))
+                if name != ".gitkeep":
+                    os.remove(os.path.join(root, name))
             for name in dirs:
                 os.rmdir(os.path.join(root, name))
 
